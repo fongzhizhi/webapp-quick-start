fada8493c39b0faf8fc883707075437142981e02
fongzhizhi <1837486996@qq.com>
2021-12-02 10:06:30 +0800

    chore: 替换commonjs插件

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 const { babel } = require("@rollup/plugin-babel");
 const resolve = require("rollup-plugin-node-resolve");
-const commonjs = require("rollup-plugin-commonjs");
+const commonjs = require("@rollup/plugin-commonjs");
 const json = require("@rollup/plugin-json");
 
 module.exports = {
