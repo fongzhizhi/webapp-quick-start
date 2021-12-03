eb03e29e5a4d1e38f08213dafc68e3ca39f338d5
fongzhizhi <1837486996@qq.com>
2021-12-03 17:23:10 +0800

    fix: 误删文件恢复

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+const { babel } = require("@rollup/plugin-babel");
+const resolve = require("rollup-plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const json = require("@rollup/plugin-json");
+
+module.exports = {
+  // 打包入口
+  input: "src/app.js",
+  // 输出配置
+  output: {
+    file: "dist/app.bundle.js",
+    format: "cjs",
+    name: "webapp-quick-start",
+  },
+  // 插件
+  plugins: [
+    // 让rollup支持第三方库的引用
+    resolve({
+      browser: true,
+    }),
+    commonjs(),
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
+      presets: ["@babel/preset-env"],
+    }),
+    json(),
+  ],
+  // 外链
+  external: [],
+  watch: {
+    include: "src/**/*.js",
+  },
+};
